d7d1da16ae96aa78b5f25fd048b66d3c89469ce4
Raphael Zillig Ricardo <raphael.zricardo@senacsp.edu.br>
2023-12-02 11:26:17 -0300

    Update App.java

--- src/main/java/org/sen/App.java
+++ src/main/java/org/sen/App.java
@@ -7,26 +7,26 @@ import org.eclipse.jetty.servlet.ServletHolder;
 public class App {
     public static void main(String[] args) {
         // Create a Jetty server that listens on port 10010
-        // Server server = new Server(10010);
+        Server server = new Server(10010);
 
         // Create a servlet context
-        // ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        // context.setContextPath("/");
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
 
         // Add a servlet that handles incoming requests
-        // context.addServlet(new ServletHolder(new MyServlet()), "/*");
+        context.addServlet(new ServletHolder(new MyServlet()), "/*");
 
-        // server.setHandler(context);
+        server.setHandler(context);
 
-        // try {
-        //     // Start the server and wait for 5 minutes
-        //     server.start();
-        //     server.join();
-        //     Thread.sleep(1 * 60 * 1000); // Sleep for 5 minutes
-        //     server.stop(); // Stop the server after 5 minutes
-        // } catch (Exception e) {
-        //     e.printStackTrace();
-        // }
-        System.out.println(“TESTE”)
+        try {
+            // Start the server and wait for 5 minutes
+            server.start();
+            server.join();
+            //Thread.sleep(1 * 60 * 1000); // Sleep for 5 minutes
+            server.stop(); // Stop the server after 5 minutes
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //System.out.println(“TESTE”)
     }
 }
