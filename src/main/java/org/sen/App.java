23648cbc1fd0e390c8508b425738378bdc77069f
Raphael Zillig Ricardo <raphael.zricardo@senacsp.edu.br>
2023-12-02 11:38:48 -0300

    Update App.java

--- src/main/java/org/sen/App.java
+++ src/main/java/org/sen/App.java
@@ -5,7 +5,26 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
 public class App {
+
+    private static final Logger logger = Logger.getLogger(ExemploLog.class.getName());
+
     public static void main(String[] args) {
+
+        // Configurando o console handler
+        ConsoleHandler consoleHandler = new ConsoleHandler();
+        consoleHandler.setLevel(Level.ALL);
+
+        // Adicionando o console handler ao logger
+        logger.addHandler(consoleHandler);
+
+        // Definindo o nível de log para o logger
+        logger.setLevel(Level.ALL);
+
+        // Exemplos de mensagens de log
+        logger.info("Isso é uma mensagem de log de severidade 'info'");
+
+        Thread.sleep(1 * 60 * 1000);
+        
         // Create a Jetty server that listens on port 10010
         // Server server = new Server(10010);
 
