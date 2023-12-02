4099870e93fef59c5fe3dc2fbda309b0a310c8f4
Raphael Zillig Ricardo <raphael.zricardo@senacsp.edu.br>
2023-12-02 11:40:55 -0300

    Update App.java

--- src/main/java/org/sen/App.java
+++ src/main/java/org/sen/App.java
@@ -6,46 +6,28 @@ import org.eclipse.jetty.servlet.ServletHolder;
 
 public class App {
 
-    private static final Logger logger = Logger.getLogger(ExemploLog.class.getName());
-
     public static void main(String[] args) {
-
-        // Configurando o console handler
-        ConsoleHandler consoleHandler = new ConsoleHandler();
-        consoleHandler.setLevel(Level.ALL);
-
-        // Adicionando o console handler ao logger
-        logger.addHandler(consoleHandler);
-
-        // Definindo o nível de log para o logger
-        logger.setLevel(Level.ALL);
-
-        // Exemplos de mensagens de log
-        logger.info("Isso é uma mensagem de log de severidade 'info'");
-
-        Thread.sleep(1 * 60 * 1000);
         
         // Create a Jetty server that listens on port 10010
-        // Server server = new Server(10010);
+        Server server = new Server(10010);
 
         // Create a servlet context
-        // ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        // context.setContextPath("/");
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
 
         // Add a servlet that handles incoming requests
-        // context.addServlet(new ServletHolder(new MyServlet()), "/*");
+        context.addServlet(new ServletHolder(new MyServlet()), "/*");
 
-        // server.setHandler(context);
+        server.setHandler(context);
 
-        // try {
-        //     // Start the server and wait for 5 minutes
-        //     server.start();
-        //     server.join();
-        //     //Thread.sleep(1 * 60 * 1000); // Sleep for 5 minutes
-        //     server.stop(); // Stop the server after 5 minutes
-        // } catch (Exception e) {
-        //     e.printStackTrace();
-        // }
-        //System.out.println(“TESTE”)
+        try {
+            // Start the server and wait for 5 minutes
+            server.start();
+            server.join();
+            Thread.sleep(1 * 10 * 100); // Sleep for 5 minutes
+            server.stop(); // Stop the server after 5 minutes
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
